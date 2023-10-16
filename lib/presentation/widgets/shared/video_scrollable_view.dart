04008cbfcd791c054d95bde75f4180180c13a904
Franco Inostroza <franco.inostrozah@gmail.com>
2023-10-16 00:41:05 -0300

    :sparkles: (video-player) added video player widget

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/videos/fullscreen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -10,13 +11,17 @@ class VideoScrollableView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
-      physics: const BouncingScrollPhysics(),
       itemCount: videos.length,
       itemBuilder: (context, index) {
         final videoPost = videos[index];
 
         return Stack(
           children: [
+            SizedBox.expand(
+                child: FullScreenPlayer(
+              caption: videoPost.caption,
+              videoUrl: videoPost.videoUrl,
+            )),
             Positioned(
                 bottom: 40, right: 20, child: VideoButtons(video: videoPost))
           ],
